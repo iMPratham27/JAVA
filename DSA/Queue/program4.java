855c8aa7c30eda96b9363424dd2a9d8143539194
Prathamesh <prathameshdhadankar07@gmail.com>
2025-02-26 23:46:02 +0530

    code added

---
+++ DSA/Queue/program4.java
@@ -0,0 +1,141 @@
+
+import java.util.Scanner;
+import java.util.Queue;
+import java.util.LinkedList;
+
+class StackUsingQueue{
+
+	Queue<Integer> q = new LinkedList<Integer>();
+
+	void push(int data){
+	
+		int size = q.size();
+		q.add(data);
+
+		for(int i=0; i<size; i++){
+		
+			q.add(q.poll());
+		}
+	}
+
+	int pop(){
+		
+		if(q.isEmpty()){
+		
+			System.out.println("Stack is empty");
+			return -1;
+		}
+
+		return q.poll();
+	}
+
+	int top(){
+	
+		if(q.isEmpty()){
+		
+			System.out.println("Stack is empty");
+			return -1;
+		}
+
+		return q.peek();
+	}
+
+	boolean empty(){
+	
+		if(q.isEmpty()){
+		
+			return true;
+		}else{
+		
+			return false;
+		}
+	}
+
+	void printStack(){
+		
+		if(q.isEmpty()){
+		
+			System.out.println("Stack is empty");
+		}else{
+		
+			System.out.println(q);
+		}
+	}
+
+	public static void main(String[] args){
+	
+		StackUsingQueue stack = new StackUsingQueue();
+
+		Scanner sc = new Scanner(System.in);
+		
+		char ch;
+
+		do{
+		
+			System.out.println("1.push");
+			System.out.println("2.pop");
+			System.out.println("3.top");
+			System.out.println("4.empty");
+			System.out.println("5.printStack");
+
+			System.out.print("Enter your choice: ");
+			int choice = sc.nextInt();
+
+			switch(choice){
+			
+				case 1: {
+						System.out.print("Enter data to add: ");
+						int data = sc.nextInt();
+						stack.push(data);
+					}
+					break;
+
+				case 2: {
+						int popEle = stack.pop();
+						if(popEle != -1){
+						
+							System.out.println(popEle + " popped");
+						}
+					}
+					break;
+
+				case 3: {
+						int topEle = stack.top();
+						if(topEle != -1){
+						
+							System.out.println("top : " + topEle);
+						}
+					}
+					break;
+
+				case 4: {
+						boolean ret = stack.empty();
+						if(ret == true){
+						
+							System.out.println("Stack is empty");
+						}else{
+						
+							System.out.println("Stack is not empty");
+						}
+					}
+					break;
+
+				case 5: {
+						stack.printStack();
+					}
+					break;
+						
+				default: 
+					System.out.println("Wrong choice");
+					break;
+
+			}
+
+			System.out.print("Do you want to continue? ");
+			ch = sc.next().charAt(0);
+
+		}while(ch == 'Y' || ch == 'y');
+
+
+	}
+}
